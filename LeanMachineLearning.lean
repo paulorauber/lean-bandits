48970905ad9c01a55c3b31f53c75faa3fb1725d8
Remy Degenne <remydegenne@gmail.com>
2026-06-09 14:16:24 +0200

    mk_all

--- LeanMachineLearning.lean
+++ LeanMachineLearning.lean
@@ -1,4 +1,4 @@
-module  -- shake: keep-all
+module  -- shake: keep-all --deprecated_module: ignore
 
 public import LeanMachineLearning.MeasureTheory.Constructions.BorelSpace.MeasurableArgMax
 public import LeanMachineLearning.MeasureTheory.Constructions.Polish.StandardBorel
